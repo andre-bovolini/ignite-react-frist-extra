af3aa05f3ecf037e19eb538837c9e597467e439b
Andre Bovolini <bovoliniandre@gmail.com>
2021-03-28 15:02:30 -0300

    versao finalizada

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react';
 
 import { Button } from './components/Button';
 import { MovieCard } from './components/MovieCard';
+import { Content } from './components/Content';
+import { SideBar } from './components/SideBar';
 
 // import { SideBar } from './components/SideBar';
 // import { Content } from './components/Content';
@@ -61,34 +63,12 @@ export function App() {
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
       <nav className="sidebar">
-        <span>Watch<p>Me</p></span>
-
-        <div className="buttons-container">
-          {genres.map(genre => (
-            <Button
-              key={String(genre.id)}
-              title={genre.title}
-              iconName={genre.name}
-              onClick={() => handleClickButton(genre.id)}
-              selected={selectedGenreId === genre.id}
-            />
-          ))}
-        </div>
+        <SideBar handleClickButton={handleClickButton} genres={genres} selectedGenreId={selectedGenreId}/>
 
       </nav>
 
       <div className="container">
-        <header>
-          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
-        </header>
-
-        <main>
-          <div className="movies-list">
-            {movies.map(movie => (
-              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
-            ))}
-          </div>
-        </main>
+        <Content movies={movies} selectedGenre={selectedGenre}/>
       </div>
     </div>
   )
